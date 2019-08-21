c7bd3d191de1f968a4ea1a8713e49392e326d9cd
Antonio Schiavon <aschiavon91@gmail.com>
2019-08-21 10:59:28 -0300

    init of boilerplate

---
+++ dist/hello.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: "Hello world",
+        event: event
+    };
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaGVsbG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaGVsbG8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFYSxRQUFBLE9BQU8sR0FBRyxLQUFLLEVBQUMsS0FBc0IsRUFBZ0IsRUFBRTtJQUNuRSxPQUFPO1FBQ0wsVUFBVSxFQUFFLEdBQUc7UUFDZixJQUFJLEVBQUUsYUFBYTtRQUNuQixLQUFLLEVBQUUsS0FBSztLQUNiLENBQUE7QUFDSCxDQUFDLENBQUEifQ==
---
+++ src/hello.ts
@@ -0,0 +1,9 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+export const handler = async(event: APIGatewayEvent): Promise<any> => {
+  return {
+    statusCode: 200,
+    body: "Hello world",
+    event: event
+  }
+}
